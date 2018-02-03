84481f3ad183bc30076349a6fd6c34e804546905
Othman Mashaab <red.fantastic@hotmail.com>
2018-02-03 09:28:46 +0000

    Done.

--- application.rb
+++ application.rb
@@ -2,9 +2,10 @@ class Application
 
   def call(env)
     resp = Rack::Response.new
-    resp.write "Hello, World"
+    resp.write "Hello, my name is othman
+    soon i will finish learning ruby, then php, then nodejs and lastly django.
+    "
     resp.finish
   end
 
 end
-
